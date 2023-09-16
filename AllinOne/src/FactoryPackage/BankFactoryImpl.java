99a5d4ec1d5b52b3f8da5e98a4cbe19e98b41fee
Mahmoud Bdran <41477881+MahmoudBdran@users.noreply.github.com>
2023-09-16 20:40:12 +0300

    Update BankFactoryImpl.java

--- AllinOne/src/FactoryPackage/BankFactoryImpl.java
+++ AllinOne/src/FactoryPackage/BankFactoryImpl.java
@@ -3,6 +3,18 @@ package FactoryPackage;
 public class BankFactoryImpl implements BankFactory{
     @Override
     public IBank getBank(int bankCode) {
-        return null;
+       IBank bank = null;
+        switch (bankCode){
+            case 1234567:
+                bank=new BankA();
+                break;
+            case 111111:
+                bank= new BankB();
+                break;
+            default:
+                System.out.println("there is no bank with this credit card!");
+
+        }
+        return bank;
     }
 }
